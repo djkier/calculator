0800e51d2f45d1cbe78f41eb000eefa4d5f38d53
djkier <djjustinekier@gmail.com>
2024-08-05 20:30:33 +0800

    initial commit

---
+++ script/index.js
@@ -0,0 +1,5 @@
+const newh1 = document.createElement('h1');
+const container = document.querySelector('.container');
+
+newh1.appendChild(document.createTextNode('Hello World'))
+container.appendChild(newh1);
