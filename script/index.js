1d1c0d3ab26c7472c688eec91e744b14c938b63a
djkier <djjustinekier@gmail.com>
2024-08-06 19:22:04 +0800

    Feat: basic style, layout complete, Pending for work: js, function, input, output

--- script/index.js
+++
@@ -1,5 +0,0 @@
-const newh1 = document.createElement('h1');
-const container = document.querySelector('.container');
-
-newh1.appendChild(document.createTextNode('Hello World'))
-container.appendChild(newh1);
